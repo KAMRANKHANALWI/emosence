2522a0679071a3b075dc5ce329710a31b2deb176
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2023-11-19 22:35:17 +0530

    emotiion detection

---
+++ app.py
@@ -0,0 +1,77 @@
+# Core Pkgs
+import streamlit as st
+import altair as alt
+
+# EDA Pkgs
+import pandas as pd
+import numpy as np
+
+# Utils
+import joblib
+
+# Import Model/Pipeline
+pipe_lr = joblib.load(open("/Users/kamrankhanalwi/Desktop/project_2/App/Models/emotion_classifier.pkl", "rb"))
+
+# Functions
+# Fxn to predict emotions 
+def predict_emotions(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+# Fxn to get probability
+def get_prediction_proba(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
+
+# Emoji Dictonary
+emotions_emoji_dict = {'anger' : '😡', 'fear' : '😨', 'joy' : '😂', 'love' : '❤️', 'sadness' : '😔', 'surprise' : '😱'}
+
+def main(): 
+    st.title("Emotion Classifier App")
+    menu = ["Home", "Monitor", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home-Emotion In Text")
+
+        with st.form(key='emotion_clf_form'):
+            raw_text = st.text_area("Type Here")
+            submit_text = st.form_submit_button(label='Submit')
+
+            if submit_text:
+                col1,col2 = st.columns(2)
+
+                # Apply Fxn Here
+                prediction = predict_emotions(raw_text)
+                probability = get_prediction_proba(raw_text)
+
+                with col1:
+                    st.success("Original Text")
+                    st.write(raw_text)
+
+                    st.success("Prediction")
+                    emoji_icon = emotions_emoji_dict[prediction]
+                    st.write("{} : {}".format(prediction, emoji_icon))
+                    st.write("Confidence : {}".format(np.max(probability)))
+
+                with col2:
+                    st.success("Prediction Probability")
+                    # st.write(probability)
+                    proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
+                    # st.write(proba_df.T)
+                    proba_df_clean = proba_df.T.reset_index()
+                    proba_df_clean.columns = ['emotions', 'probability']
+
+                    fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
+                    st.altair_chart(fig, use_container_width=True)
+
+    elif choice == "Monitor":
+        st.subheader("Monitor App")
+
+    else:
+        st.subheader("About")
+
+
+
+if __name__ == '__main__':
+    main()
